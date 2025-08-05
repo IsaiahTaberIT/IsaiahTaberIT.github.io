39cbb4964c6945801981c37cdd06c8b0286e8425
IsaiahTaberIT <IsaiahTaber1@gmail.com>
2025-08-05 00:57:34 -0400

    put a bunch of work into the about page more limited work into index.html though

--- Index.js
+++ Index.js
@@ -6,3 +6,7 @@ function sayhi() {
 
 
 sayhi();
+
+//<img src="Images/julia(-1.148933,0.2095252).png" alt="Julia set fractal at position: -1.148933,0.2095252" />
+//    <a href="about.html">About me</a>
+//<a href="https://Google.com" , target="_blank">Google</a>
--- about.js
+++ about.js
@@ -10,7 +10,56 @@
     potatoElement.textContent = 'ur mum has ' + potato + ' potatoes';
 }
 
+function fetchAndSetByid(file, id) {
+    let element = document.getElementById(id)
+
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+
+            element.innerHTML = data;
+
+        })
+        .catch(error => {
+            console.error('There was a problem fetching the file:', error);
+        });
+}
+
+fetchAndSetByid("education", "education")
+fetchAndSetByid("background", "background")
+
 const potatoElement = document.getElementById("potatocount")
+const aboutMetxt = document.getElementById("aboutme")
+const currentDate = new Date();
+const currentyear = currentDate.getFullYear() * 1;
+
+let agetext = 'My name is Isaiah Taber I\'m ' +  (currentyear - 2004) + ' years old and, ';
+
+
+fetch('aboutmetext')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text(); // Parse the response as text
+    })
+    .then(data => {
+        // Now 'data' holds the text from the file
+        console.log(data);
+        aboutMetxt.textContent = agetext + data;
+        // You can display it in an HTML element, e.g.:
+        // document.getElementById('outputDiv').textContent = data;
+    })
+    .catch(error => {
+        console.error('There was a problem fetching the file:', error);
+    });
+
+//aboutMetxt.textContent = text;
 
 
 
@@ -18,4 +67,4 @@ let potato = 0;
 let w1 = 1;
 let w2 = 0;
 
-sayUrMum();
+//sayUrMum();
